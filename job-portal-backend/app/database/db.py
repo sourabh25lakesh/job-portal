dde13d1c146d4119f9ebf593fc025fe351f26932
Admin <admin123@example.com>
2026-07-14 12:51:59 +0530

    Debug DATABASE_URL

--- job-portal-backend/app/database/db.py
+++ job-portal-backend/app/database/db.py
@@ -1,12 +1,11 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 from app.core.config import DATABASE_URL
 
-engine = create_engine(
-    DATABASE_URL
-)
+print("DATABASE_URL =", repr(DATABASE_URL))
+
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -18,13 +17,8 @@ Base = declarative_base()
 
 
 def get_db():
-
     db = SessionLocal()
-
     try:
-
         yield db
-
     finally:
-
         db.close()
